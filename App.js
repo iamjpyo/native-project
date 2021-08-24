77c55d7ff72fa31e5b76d0936e2197d09f5524dc
Juan Pablo <jpluncumilla@gmail.com>
2021-08-23 23:01:05 -0400

    Minimal flexbox to input form

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={{padding: 50}}>
-      <View>
-        <TextInput placeholder="Developer Goal" style={{borderBottomColor: "black", borderBottomWidth: 1, padding: 10}} />
+      <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+        <TextInput placeholder="Developer Goal" style={{width: "75%", borderBottomColor: "black", borderBottomWidth: 1, padding: 10}} />
         <Button title="ADD"/>
       </View>
       <View>
